8c6bb9b578f279a681427f32fed6340690d214f3
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-10 20:35:17 +0200

    Data Types

--- hello.py
+++ hello.py
@@ -1,1 +1,15 @@
-print("Hello Python")
+# Data Types
+
+print(type(20)) # int => Intager
+
+print(type(2.0)) # float => Float
+
+print(type("Mostafa")) # str => String
+
+print(type(True)) # bool => Boolean
+
+print(type([1, 2, 3])) # list => List
+
+print(type((1, 2, 3))) # tuple => Tuple
+
+print(type({"One": 1, "Two": 2, "Three": 3})) # dict => Dictionary
