6557f714c224d823d6c48eeb7db4e9913a825e0a
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-10 21:47:09 +0200

    Strings lesson 2

--- hello.py
+++ hello.py
@@ -1,26 +1,36 @@
-# -------------
-# -- Strings --
-# -------------
+# ---------------------------------
+# Strings Indexing & Slicing
+# [1] All Data in Python is Object
+# [2] Object Contain Elements
+# [3] Every Element Has Its Own Index
+# [4] Python Use Zero Based Indexing ( Index Start From Zero )
+# [5] Use Square Brackets To Access Element
+# [6] Enable Accessing Parts Of Strings, Tuples or Lists
+# ---------------------------------
 
-myStringOne = 'This is Single Quote'
-myStringTwo = "This is Double Quotes"
+# Indexing ( Access Single Item )
 
-print(myStringOne)
-print(myStringTwo)
+myString = "I Love Python"
 
-myStringThree = 'This is Single Quote "Test"'
-myStringFour = "This is Double Quotes 'Test'"
+print(myString[0])  # Index 0 => I
+print(myString[9])  # Index 9 => t
 
-print(myStringThree)
-print(myStringFour)
+print(myString[-1])  # Index -1 => First Character From End
+print(myString[-6])  # Index -6 => 6th Character From End
 
-myStringFive = '''First
-Second 'Test' "Test"
-Third'''
+# Slicing ( Access Multiple Sequence Items )
+# [Start:End] End Not Included
+# [Start:End:Steps]
 
-myStringSix = """First
-Second "Test" \\\ 'Test'
-Third"""
+print(myString[8:11])  # yth
+print(myString[3:5])  # ov
 
-print(myStringFive)
-print(myStringSix)
+print(myString[:10])  # If Start Is Not Here Will Start From 0 (I Love Pyt)
+print(myString[5:])  # If End Is Not Here Will Go To The End (e Python)
+print(myString[:])  # Full Data
+
+print(myString[0::1])  # Full Data
+print(myString[::1])  # Full Data
+
+print(myString[::2])  # Steps
+print(myString[::3])  # Steps
