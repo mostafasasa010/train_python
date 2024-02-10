8be5b7f18670f27773a2568258fdd6dab83b6aac
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-10 22:49:15 +0200

    Strings lesson 6

--- hello.py
+++ hello.py
@@ -2,76 +2,17 @@
 # -- Strings Methods --
 # ---------------------
 
-# index(SubString, Start, End)
+# replace(Old Value, New Value, Count)
 
-a = "I Love Python"
-# print(a.index("P"))  # Index Number 7
-# print(a.index("P", 0, 10))  # Index Number 7
-# print(a.index("P", 0, 5))  # Through Error
+a = "Hello One Two Three One One"
+print(a.replace("One", "1"))
+print(a.replace("One", "1", 1))
+print(a.replace("One", "1", 2))
 
-# find(SubString, Start, End)
+# join(Iterable)
 
-b = "I Love Python"
-print(b.find("P"))  # Index Number 7
-print(b.find("P", 0, 10))  # Index Number 7
-print(b.find("P", 0, 5))  # -1
-
-# rjust(Width, Fill Char) ljust(Width, Fill Char)
-
-c = "Osama"
-print(c.rjust(10))
-print(c.rjust(10, "#"))
-
-d = "Osama"
-print(d.ljust(10))
-print(d.ljust(10, "#"))
-
-# splitlines()
-
-e = """First Line
-Second Line
-Third Line"""
-
-print(e.splitlines())
-
-f = "First Line\nSecond Line\nThird Line"
-
-print(f.splitlines())
-
-# expandtabs()
-
-g = "Hello\tWorld\tI\tLove\tPython"
-print(g.expandtabs(2))
-
-one = "I Love Python And 3G"
-two = "I Love Python And 3g"
-print(one.istitle())
-print(two.istitle())
-
-three = " "
-four = ""
-print(three.isspace())
-print(four.isspace())
-
-five = 'i love python'
-six = 'I Love Python'
-print(five.islower())
-print(six.islower())
-
-seven = "osama_elzero"
-eight = "OsamaElzero100"
-nine = "Osama--Elzero100"
-
-print(seven.isidentifier())
-print(eight.isidentifier())
-print(nine.isidentifier())
-
-x = "AaaaaBbbbbb"
-y = "AaaaaBbbbbb111"
-print(x.isalpha())
-print(y.isalpha())
-
-u = "AaaaaBbbbbb"
-z = "AaaaaBbbbbb111"
-print(u.isalnum())
-print(z.isalnum())
+myList = ["Osama", "Mohamed", "Elsayed"]
+print("-".join(myList))
+print(" ".join(myList))
+print(", ".join(myList))
+print(type(", ".join(myList)))
