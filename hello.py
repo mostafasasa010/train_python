3c802e487db3f528e3a76b6982447530590a8392
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-11 16:38:31 +0200

    Lists Lesson 3

--- hello.py
+++ hello.py
@@ -2,49 +2,44 @@
 # -- Lists Methods --
 # -------------------
 
-# append()
-
-myFriends = ["Osama", "Ahmed", "Sayed"]
-myOldFriends = ["Haytham", "Samah", "Ali"]
-
-myFriends.append("Alaa")
-myFriends.append(100)
-myFriends.append(150.200)
-myFriends.append(True)
-myFriends.append(myOldFriends)
-
-print(myFriends)
-print(myFriends[2])
-print(myFriends[6])
-print(myFriends[7])
-print(myFriends[7][2])
-
-# extend()
+# clear()
 
 a = [1, 2, 3, 4]
-b = ["A", "B", "C"]
-c = ["One", "Two"]
-
-a.extend(b)
-a.extend(c)
-
+a.clear()
 print(a)
 
-# remove()
+# copy()
 
-x = [1, 2, 3, 4, 5, "Osama", True, "Osama", "Osama"]
-x.remove("Osama")
-print(x)
+b = [1, 2, 3, 4]
+c = b.copy()
 
-# sort()
+print(b)  # Main List
+print(c)  # Copied List
 
-y = [1, 2, 100, 120, -10, 17, 29]
-# y = ["A", "Z", "C"]
-y.sort(reverse=True)
-print(y)
+b.append(5)
 
-# reverse()
+print(b)  # Main List
+print(c)  # Copied List
 
-z = [10, 1, 9, 80, 100, "Osama", 100]
-z.reverse()
-print(z)
+# count()
+
+d = [1, 2, 3, 4, 3, 9, 10, 1, 2, 1]
+print(d.count(1))
+
+# index()
+
+e = ["Osama", "Ahmed", "Sayed", "Ramy", "Ahmed", "Ramy"]
+print(e.index("Ramy"))
+
+# insert()
+
+f = [1, 2, 3, 4, 5, "A", "B"]
+f.insert(0, "Test")
+f.insert(-1, "Test")
+
+print(f)
+
+# pop()
+
+g = [1, 2, 3, 4, 5, "A", "B"]
+print(g.pop(-3))
