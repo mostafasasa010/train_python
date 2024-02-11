e399ef3a849c858ec6e6dccce7707dbfb97e2dd4
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-11 16:55:32 +0200

    Tuple Lesson 2

--- hello.py
+++ hello.py
@@ -1,41 +1,60 @@
-# -----------------------------
+# -----------
 # -- Tuple --
 # -----------
-# [1] Tuple Items Are Enclosed in Parentheses
-# [2] You Can Remove The Parentheses If You Want
-# [3] Tuple Are Ordered, To Use Index To Access Item
-# [4] Tuple Are Immutable => You Cant Add or Delete (The Main Different From List)
-# [5] Tuple Items Is Not Unique
-# [6] Tuple Can Have Different Data Types
-# [7] Operators Used in Strings and Lists Available In Tuples
-# -----------------------------
 
-# Tuple Syntax & Type Test
+# Tuple With One Element
 
-myAwesomeTupleOne = ("Osama", "Ahmed")
-myAwesomeTupleTwo = "Osama", "Ahmed"
+myTuple1 = ("Osama",)
+myTuple2 = "Osama",
 
-print(myAwesomeTupleOne)
-print(myAwesomeTupleTwo)
+print(myTuple1)
+print(myTuple2)
 
-print(type(myAwesomeTupleOne))
-print(type(myAwesomeTupleTwo))
+print(type(myTuple1))
+print(type(myTuple2))
 
-# Tuple Indexing
+print(len(myTuple1))
+print(len(myTuple2))
 
-myAwesomeTupleThree = (1, 2, 3, 4, 5)
-print(myAwesomeTupleThree[0])
-print(myAwesomeTupleThree[-1])
-print(myAwesomeTupleThree[-3])
+# Tuple Concatenation
 
-# Tuple Assign Values
+a = (1, 2, 3, 4)
+b = (5, 6)
 
-myAwesomeTupleFour = (1, 2, 3, 4, 5)
-# myAwesomeTupleFour[2] = "Three"
-# print(myAwesomeTupleFour)  # 'tuple' object does not support item assignment
+c = a + b
+d = a + ("A", "B", True) + b
 
-# Tuple Data
+print(c)
+print(d)
 
-myAwesomeTupleFive = ("Osama", "Osama", 1, 2, 3, 100.5, True)
-print(myAwesomeTupleFive[1])
-print(myAwesomeTupleFive[-1])
+# Tuple, List, String Repeat (*)
+
+myString = "Osama"
+myList = [1, 2]
+myTuple = ("A", "B")
+
+print(myString * 6)
+print(myList * 6)
+print(myTuple * 6)
+
+# Methods => count()
+
+a = (1, 3, 7, 8, 2, 6, 5, 8)
+print(a.count(8))
+
+# Methods => index()
+
+b = (1, 3, 7, 8, 2, 6, 5)
+# print("The Position of Index Is: " + b.index(7))  # Error
+print("The Position of Index Is: {:d}".format(b.index(7)))
+print(f"The Position of Index Is: {b.index(7)}")
+
+# Tuple Destruct
+
+a = ("A", "B", 4, "C")
+
+x, y, _, z = a
+
+print(x)
+print(y)
+print(z)
