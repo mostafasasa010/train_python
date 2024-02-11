5d40cceeb9398b9860c5db90048adfd3d245451c
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-11 18:41:47 +0200

    Dictionary Lesson 3

--- hello.py
+++ hello.py
@@ -2,43 +2,47 @@
 # -- Dictionary Methods --
 # ------------------------
 
-# clear()
+# setdefault()
 
 user = {
   "name": "Osama"
 }
 print(user)
-user.clear()
+print(user.setdefault("age", 36))
 print(user)
 
-print("=" * 50)
+print("=" * 40)
 
-# update()
+# popitem()
 
 member = {
-  "name": "Osama"
+  "name": "Osama",
+  "skill": "PS4"
 }
 print(member)
-member["age"] = 36
-print(member)
-member.update({"country": "Egypt"})
-print(member)
+member.update({"age": 36})
+print(member.popitem())
 
-print("=" * 50)
+print("=" * 40)
 
-# copy()
+# items()
 
-main = {
-  "name": "Osama"
+view = {
+  "name": "Osama",
+  "skill": "XBox"
 }
 
-b = main.copy()
-print(b)
-main.update({"skills": "Fighting"})
-print(main)
-print(b)
+allItems = view.items()
+print(view)
+view["age"] = 36
 
-# keys() + values()
+print(allItems)
 
-print(main.keys())
-print(main.values())
+print("=" * 40)
+
+# fromkeys()
+
+a = ('MyKeyOne', 'MyKeyTwo', 'MyKeyThree')
+b = "X"
+
+print(dict.fromkeys(a, b))
