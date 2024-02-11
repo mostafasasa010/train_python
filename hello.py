26cca5940ea53417bd9e77f749400384cd2aa976
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-11 17:12:04 +0200

    Set Lesson 2

--- hello.py
+++ hello.py
@@ -1,32 +1,66 @@
-# -----------------------------
-# -- Set --
-# ---------
-# [1] Set Items Are Enclosed in Curly Braces
-# [2] Set Items Are Not Ordered And Not Indexed
-# [3] Set Indexing and Slicing Cant Be Done
-# [4] Set Has Only Immutable Data Types (Numbers, Strings, Tuples) List and Dict Are Not
-# [5] Set Items Is Unique
-# -----------------------------
+# -----------------
+# -- Set Methods --
+# -----------------
 
-# Not Ordered And Not Indexed
+# clear()
 
-mySetOne = {"Osama", "Ahmed", 100}
-print(mySetOne)
-# print(mySetOne[0])
+a = {1, 2, 3}
+a.clear()
+print(a)
 
-# Slicing Cant Be Done
+# union()
 
-mySetTwo = {1, 2, 3, 4, 5, 6}
-# print(mySetTwo[0:3])
+b = {"One", "Two", "Three"}
+c = {"1", "2", "3"}
+x = {"Zero", "Cool"}
 
-# Has Only Immutable Data Types
+print(b | c)
+print(b.union(c, x))
 
-# mySetThree = {"Osama", 100, 100.5, True, [1, 2, 3]} # unhashable type: 'list'
-mySetThree = {"Osama", 100, 100.5, True, (1, 2, 3)}
+# add()
 
-print(mySetThree)
+d = {1, 2, 3, 4}
+d.add(5)
+d.add(6)
+print(d)
 
-# Items Is Unique
+# copy()
 
-mySetFour = {1, 2, "Osama", "One", "Osama", 1}
-print(mySetFour)
+e = {1, 2, 3, 4}
+f = e.copy()
+
+print(e)
+print(f)
+
+e.add(6)
+
+print(e)
+print(f)
+
+# remove()
+
+g = {1, 2, 3, 4}
+g.remove(1)
+# g.remove(7) # Error
+print(g)
+
+# discard()
+
+h = {1, 2, 3, 4}
+h.discard(1)
+h.discard(7)
+print(h)
+
+# pop()
+
+i = {"A", True, 1, 2, 3, 4, 5}
+print(i.pop())
+
+# update()
+
+j = {1, 2, 3}
+k = {1, "A", "B", 2}
+j.update(['Html', "Css"])
+j.update(k)
+
+print(j)
