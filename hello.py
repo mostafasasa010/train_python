5d1a71c154027860dbf53e591091d6bd781694fc
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-11 16:23:11 +0200

    Lists Lesson 2

--- hello.py
+++ hello.py
@@ -1,29 +1,50 @@
-# -----------------------------
-# -- Lists --
-# -----------
-# [1] List Items Are Enclosed in Square Brackets
-# [2] List Are Ordered, To Use Index To Access Item
-# [3] List Are Mutable => Add, Delete, Edit
-# [4] List Items Is Not Unique
-# [5] List Can Have Different Data Types
-# -----------------------------
+# -------------------
+# -- Lists Methods --
+# -------------------
 
-myAwesomeList = ["One", "Two", "One", 1, 100.5, True]
+# append()
 
-print(myAwesomeList)  # Whole List
-print(myAwesomeList[1])  # "Two"
-print(myAwesomeList[-1])  # True
-print(myAwesomeList[-3])  # 1
+myFriends = ["Osama", "Ahmed", "Sayed"]
+myOldFriends = ["Haytham", "Samah", "Ali"]
 
-print(myAwesomeList[1:4])  # ['Two', 'One', 1]
-print(myAwesomeList[:4])  # ['One', 'Two', 'One', 1]
-print(myAwesomeList[1:])  # ['Two', 'One', 1, 100.5, True]
+myFriends.append("Alaa")
+myFriends.append(100)
+myFriends.append(150.200)
+myFriends.append(True)
+myFriends.append(myOldFriends)
 
-print(myAwesomeList[::1])  # ['One', 'Two', 'One', 1, 100.5, True]
-print(myAwesomeList[::2])  # ['One', 'One', 100.5]
+print(myFriends)
+print(myFriends[2])
+print(myFriends[6])
+print(myFriends[7])
+print(myFriends[7][2])
 
-print(myAwesomeList)
-# myAwesomeList[1] = 2
-# myAwesomeList[-1] = False
-myAwesomeList[0:3] = ["A"]
-print(myAwesomeList)
+# extend()
+
+a = [1, 2, 3, 4]
+b = ["A", "B", "C"]
+c = ["One", "Two"]
+
+a.extend(b)
+a.extend(c)
+
+print(a)
+
+# remove()
+
+x = [1, 2, 3, 4, 5, "Osama", True, "Osama", "Osama"]
+x.remove("Osama")
+print(x)
+
+# sort()
+
+y = [1, 2, 100, 120, -10, 17, 29]
+# y = ["A", "Z", "C"]
+y.sort(reverse=True)
+print(y)
+
+# reverse()
+
+z = [10, 1, 9, 80, 100, "Osama", 100]
+z.reverse()
+print(z)
