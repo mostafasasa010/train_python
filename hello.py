5110bc968a15f0c081a8a0db0f602b517c449318
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-11 18:35:52 +0200

    Dictionary Lesson 2

--- hello.py
+++ hello.py
@@ -1,78 +1,44 @@
-# ---------------------------
-# -- Dictionary --
-# ----------------
-# [1] Dict Items Are Enclosed in Curly Braces
-# [2] Dict Items Are Contains Key : Value
-# [3] Dict Key Need To Be Immutable => (Number, String, Tuple) List Not Allowed
-# [4] Dict Value Can Have Any Data Types
-# [5] Dict Key Need To Be Unique
-# [6] Dict Is Not Ordered You Access Its Element With Key
-# ----------------------------
+# ------------------------
+# -- Dictionary Methods --
+# ------------------------
 
-# Dictionary
+# clear()
 
 user = {
-  "name": "Osama",
-  "age": 36,
-  "country": "Egypt",
-  "skills": ["Html", "Css", "JS"],
-  "rating": 10.5
+  "name": "Osama"
 }
-
 print(user)
-print(user['country'])
-print(user.get("country"))
+user.clear()
+print(user)
 
-print(user.keys())
-print(user.values())
+print("=" * 50)
 
-# Two-Dimensional Dictionary
+# update()
 
-languages = {
-  "One": {
-    "name": "Html",
-    "progress": "80%"
-  },
-  "Two": {
-    "name": "Css",
-    "progress": "90%"
-  },
-  "Three": {
-    "name": "Js",
-    "progress": "90%"
-  }
+member = {
+  "name": "Osama"
+}
+print(member)
+member["age"] = 36
+print(member)
+member.update({"country": "Egypt"})
+print(member)
+
+print("=" * 50)
+
+# copy()
+
+main = {
+  "name": "Osama"
 }
 
-print(languages)
-print(languages['One'])
-print(languages['Three']['name'])
+b = main.copy()
+print(b)
+main.update({"skills": "Fighting"})
+print(main)
+print(b)
 
-# Dictionary Length
+# keys() + values()
 
-print(len(languages))
-print(len(languages["Two"]))
-
-# Create Dictionary From Variables
-
-frameworkOne = {
-  "name": "Vuejs",
-  "progress": "80%"
-}
-
-frameworkTwo = {
-  "name": "ReactJs",
-  "progress": "80%"
-}
-
-frameworkThree = {
-  "name": "Angular",
-  "progress": "80%"
-}
-
-allFramework = {
-  "one": frameworkOne,
-  "two": frameworkTwo,
-  "three": frameworkThree
-}
-
-print(allFramework)
+print(main.keys())
+print(main.values())
