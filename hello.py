e989b64be8cea5268a4f67ea7466e9e585dbfdf6
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-13 18:49:05 +0200

    Example

--- hello.py
+++ hello.py
@@ -1,6 +1,18 @@
-email = input("Enter Your Email: ")
+age = input("Enter Your Age Years: ")
 
-userName = email.strip().split("@")[0]
-emailTag = email.strip().split("@")[1].split(".")[0]
+age = int(age.strip())
+months = age * 12
+weeks = months * 4
+days = weeks * 7
+hours = days * 24
+minutes = hours * 60
+seconds = minutes * 60
 
-print(f"User Name Is {userName}, Email Tag Is {emailTag}")
+print(f"""
+  Your Count Months Is: {months}
+  Your Count Weeks Is: {weeks:,}
+  Your Count Days Is: {days:,}
+  Your Count Hours Is: {hours:,}
+  Your Count Minutes Is: {minutes:,}
+  Your Count Seconds Is: {seconds:,}
+""")
