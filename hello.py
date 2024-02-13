6e7e2d7298a7819b33681566418dc7b3d07603f7
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-13 17:47:13 +0200

    User Input

--- hello.py
+++ hello.py
@@ -1,59 +1,13 @@
-# ---------------------
-# -- Type Conversion --
-# ----------------------
+# ----------------
+# -- User Input --
+# ----------------
 
-# str()
+fName = input('What\'s Is Your First Name?')
+mName = input('What\'s Is Your Middle Name?')
+lName = input('What\'s Is Your Last Name?')
 
-a = 10
-print(type(a))
-print(type(str(a)))
+fName = fName.strip().capitalize()
+mName = mName.strip().capitalize()
+lName = lName.strip().capitalize()
 
-print("#" * 50)
-
-# tuple()
-
-c = "Osama"  # String
-d = [1, 2, 3, 4, 5]  # List
-e = {"A", "B", "C"}  # Set
-f = {"A": 1, "B": 2}  # Dictionary
-
-print(tuple(c))
-print(tuple(d))
-print(tuple(e))
-print(tuple(f))
-
-# list()
-
-c = "Osama"  # String
-d = (1, 2, 3, 4, 5)  # Tuple
-e = {"A", "B", "C"}  # Set
-f = {"A": 1, "B": 2}  # Dictionary
-
-print(list(c))
-print(list(d))
-print(list(e))
-print(list(f))
-
-print("#" * 50)
-
-# set()
-
-c = "Osama"  # String
-d = (1, 2, 3, 4, 5)  # Tuple
-e = ["A", "B", "C"]  # List
-f = {"A": 1, "B": 2}  # Dictionary
-
-print(set(c))
-print(set(d))
-print(set(e))
-print(set(f))
-
-print("#" * 50)
-
-# dict()
-
-d = (("A", 1), ("B", 2), ("C", 3))  # Tuple
-e = [["One", 1], ["Two", 2], ["Three", 3]]  # List
-
-print(dict(d))
-print(dict(e))
+print(f"Hello {fName} {mName:.1s} {lName} Happy To See You.")
