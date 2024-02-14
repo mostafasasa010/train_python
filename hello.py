61b0132e48b80cb435fbfe0d34b52e89505edbab
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-14 20:28:38 +0200

    Loop Lesson 1

--- hello.py
+++ hello.py
@@ -1,27 +1,20 @@
-admins = ["Mostafa", "Ahmed", "Mohamad", "Abdalla", "Mai", "Nada", "Habiba"]
+# -------------------
+# -- Loop => While --
+# -------------------
+# while condition_is_true
+#   Code Will Run Until Condition Become False
+# -----------------------
 
-name = input("Enter Your Name Please: ").strip().capitalize()
+a = 0
 
-if name in admins:
-  print("You Are Included Admins :)")
-  print("Update => U \nDelete => D")
-  option = input("Are You Update Or Delete Name? ").strip().capitalize()
-  if option == "Update" or option == "U":
-    newName = input("Enter Your New Name Please :) ").strip().capitalize()
-    admins[admins.index(name)] = newName
-    print(f"Thank You :) \nAdmins: {admins}")
-  elif option == "Delete" or option == "D":
-    admins.remove(name)
-    print(f"Done |-_-| \nAdmins: {admins}")
-  else:
-    print("Option Not Valid")
-else:
-  print("Your Name Not Included Admins -_-")
-  print("Yes => Y \nNo => N")
-  addYou = input("Are You Add To Admins? Yes, No: ").strip().capitalize()
-  if addYou == "Yes" or addYou == "Y":
-    print("Congratulations Add You To Admins :)")
-    admins.append(name)
-    print(f"Thank You :) \nAdmins: {admins}")
-  else:
-    print("Your Not Included Admins -_-")
+while a < 15:
+
+  print(a)
+
+  a += 1  # a = a + 1
+
+print("Loop is Done")  # True Become False
+
+while False:
+
+  print("Will Not Print")
