01bff6fea947cfe3de50a349f4a06e8206773a1a
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-14 19:06:38 +0200

    Membership Lesson 1

--- hello.py
+++ hello.py
@@ -1,7 +1,46 @@
-age = int(input("Enter Your Age Years: ").strip())
-unit = input("Enter Unit From: (Month, Week, Day) ").strip().lower()
+# --------------------------
+# -- Membership Operators --
+# --------------------------
+# in
+# not in
+# --------------------------
 
-if unit == "month": print(f"You Lived {(age * 12):,} Months.")
-elif unit == "week": print(f"You Lived {(age * 48):,} Weeks.")
-elif unit == "day": print(f"You Lived {(age * 365):,} Days.")
-else: print(f"Not Valid Unit")
+# String
+
+name = "Osama"
+print("s" in name)
+print("a" in name)
+print("A" in name)
+
+print("#" * 50)
+
+# List
+
+friends = ["Ahmed", "Sayed", "Mahmoud"]
+print("Osama" in friends)
+print("Sayed" in friends)
+print("Mahmoud" not in friends)
+
+print("#" * 50)
+
+# Using In and Not In With Condition
+
+countriesOne = ["Egypt", "KSA", "Kuwait", "Bahrain", "Syria"]
+countriesOneDiscount = 80
+
+countriesTwo = ["Italy", "USA"]
+countriesTwoDiscount = 50
+
+myCountry = "Italy"
+
+if myCountry in countriesOne:
+
+  print(f"Hello You Have A Discount Equal To ${countriesOneDiscount}")
+
+elif myCountry in countriesTwo:
+
+  print(f"Hello You Have A Discount Equal To ${countriesTwoDiscount}")
+
+else:
+
+  print("You Have No Discount")
