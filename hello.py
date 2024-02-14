6d8b7fc895cebd70968f9af578957c0df76c693a
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-14 20:32:15 +0200

    Loop Lesson 2

--- hello.py
+++ hello.py
@@ -1,20 +1,33 @@
-# -------------------
-# -- Loop => While --
-# -------------------
+# ----------------------------
+# -- Loop => While Training --
+# ----------------------------
 # while condition_is_true
 #   Code Will Run Until Condition Become False
 # -----------------------
 
+myF = ["Os", "Ah", "Ga", "Al", "Ra", "Sa", "Ta", "Ma", "Mo", "Wa"]
+
+# print(len(myF))  # List Length [10]
+
 a = 0
 
-while a < 15:
+while a < len(myF):  # a < 10
 
-  print(a)
+  print(f"#{str(a + 1).zfill(3)} {myF[a]}")
 
   a += 1  # a = a + 1
 
-print("Loop is Done")  # True Become False
+else:
 
-while False:
+  print("All Friends Printed To Screen.")
 
-  print("Will Not Print")
+# print(myF[0])
+# print(myF[1])
+# print(myF[2])
+# print(myF[3])
+# print(myF[4])
+# print(myF[5])
+# print(myF[6])
+# print(myF[7])
+# print(myF[8])
+# print(myF[9])
