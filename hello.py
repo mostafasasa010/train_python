2fbd70605a64cf13d0690b682594f722c634310e
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-14 19:45:32 +0200

    Membership Example

--- hello.py
+++ hello.py
@@ -1,46 +1,27 @@
-# --------------------------
-# -- Membership Operators --
-# --------------------------
-# in
-# not in
-# --------------------------
+admins = ["Mostafa", "Ahmed", "Mohamad", "Abdalla", "Mai", "Nada", "Habiba"]
 
-# String
+name = input("Enter Your Name Please: ").strip().capitalize()
 
-name = "Osama"
-print("s" in name)
-print("a" in name)
-print("A" in name)
-
-print("#" * 50)
-
-# List
-
-friends = ["Ahmed", "Sayed", "Mahmoud"]
-print("Osama" in friends)
-print("Sayed" in friends)
-print("Mahmoud" not in friends)
-
-print("#" * 50)
-
-# Using In and Not In With Condition
-
-countriesOne = ["Egypt", "KSA", "Kuwait", "Bahrain", "Syria"]
-countriesOneDiscount = 80
-
-countriesTwo = ["Italy", "USA"]
-countriesTwoDiscount = 50
-
-myCountry = "Italy"
-
-if myCountry in countriesOne:
-
-  print(f"Hello You Have A Discount Equal To ${countriesOneDiscount}")
-
-elif myCountry in countriesTwo:
-
-  print(f"Hello You Have A Discount Equal To ${countriesTwoDiscount}")
-
+if name in admins:
+  print("You Are Included Admins :)")
+  print("Update => U \nDelete => D")
+  option = input("Are You Update Or Delete Name? ").strip().capitalize()
+  if option == "Update" or option == "U":
+    newName = input("Enter Your New Name Please :) ").strip().capitalize()
+    admins[admins.index(name)] = newName
+    print(f"Thank You :) \nAdmins: {admins}")
+  elif option == "Delete" or option == "D":
+    admins.remove(name)
+    print(f"Done |-_-| \nAdmins: {admins}")
+  else:
+    print("Option Not Valid")
 else:
-
-  print("You Have No Discount")
+  print("Your Name Not Included Admins -_-")
+  print("Yes => Y \nNo => N")
+  addYou = input("Are You Add To Admins? Yes, No: ").strip().capitalize()
+  if addYou == "Yes" or addYou == "Y":
+    print("Congratulations Add You To Admins :)")
+    admins.append(name)
+    print(f"Thank You :) \nAdmins: {admins}")
+  else:
+    print("Your Not Included Admins -_-")
