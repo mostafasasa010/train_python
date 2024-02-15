6409445755a896a041705f6d53fb1ff6b42229e2
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-15 20:37:20 +0200

    Function Lesson 9

--- hello.py
+++ hello.py
@@ -1,10 +1,23 @@
-# WWWoooorrrldd = World
+# ------------------------
+# -- Function => lambda --
+# -- Anonymous Function --
+# ------------------------
+# [1] It Has No Name
+# [2] You Can Call It Inline Without Defining It
+# [3] You Can Use It In Return Data From Another Function
+# [4] Lambda Used For Simple Functions and Def Handle The Large Tasks
+# [5] Lambda is One Single Expression not Block Of Code
+# [6] Lambda Type is Function
+# -------------------------------------------------------------------
 
-def clean_word(word):
-  if len(word) == 1:
-    return word
-  if word[0] == word[1]:
-    return clean_word(word[1:])
-  return word[0] + clean_word(word[1:])
+def say_hello(name, age) : return f"Hello {name} your Age Is: {age}"
 
-print(clean_word("WWWoooorrrldd"))
+hello = lambda name, age : f"Hello {name} your Age Is: {age}"
+
+print(say_hello("Ahmed", 36))
+print(hello("Ahmed", 36))
+
+print(say_hello.__name__)
+print(hello.__name__)
+
+print(type(hello))
