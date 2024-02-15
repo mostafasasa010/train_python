7f6628c7bf3a0d3907859ec3cd1922705deea5db
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-15 19:39:03 +0200

    Function Lesson 4

--- hello.py
+++ hello.py
@@ -1,29 +1,13 @@
-# -------------------------------------------------
-# -- Function Packing, Unpacking Arguments *Args --
-# -------------------------------------------------
+# ---------------------------------
+# -- Function Default Parameters --
+# ---------------------------------
 
-print(1, 2, 3, 4)
+def say_hello(name="Unknown", age="Unknown", country="Unknown"):
 
-myList = [1, 2, 3, 5]
+  print(f"Hello {name} Your Age is {age} and Your Country Is {country}")
 
-print(myList)
-print(*myList)
-
-def say_hello(*peoples):  # n1, n2, n3, n4
-
-  for name in peoples:
-
-    print(f"Hello {name}")
-
-say_hello("Osama", "Ahmed", "Sayed", "Mahmoud")
-
-def show_details(name, *skills):
-
-  print(f"Hello {name} Your Skills Is: ")
-
-  for skill in skills:
-
-    print(skill)
-
-show_details("Osama", "Html", "CSS", "JS")
-show_details("Ahmed", "Html", "CSS", "JS", "Python", "PHP", "MySQL")
+say_hello("Osama", 36, "Egypt")
+say_hello("Mahmoud", 28, "KSA")
+say_hello("Sameh", 38)
+say_hello("Ramy")
+say_hello()
