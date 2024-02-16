1e5d391b83cf8c3b0ac913f0f705fc812716dd1d
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-16 19:57:39 +0200

    Assignment from 69 to 71 => 2

--- hello.py
+++ hello.py
@@ -1,20 +1,5 @@
-values = (0, 1, 2)
+v = 40
 
-if any(values):
-  # my_var become 0 because values included 1, 2 becouse those are true
-  my_var = 0
+my_range = list(range(v))
 
-my_list = [True, 1,  1, ["A", "B"], 10.5, my_var]
-
-# my_list[:4] = true
-# my_list[:6] = false
-# my_list[:] = false
-if all(my_list[:4]) or all(my_list[:6]) or all(my_list[:]):
-  # print Good because my_list[:4] = true
-  print("Good")
-
-else:
-
-  print("Bad")
-
-# Output = Good
+print(sum(my_range, v) + pow(v, v, v))  # 820
