2bf855ba2c4aa21e7a923c86864d0f00e695fbad
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-16 16:05:44 +0200

    Assignment from 60 to 64 => 1

--- hello.py
+++ hello.py
@@ -1,23 +1,9 @@
-# ------------------------
-# -- Function => lambda --
-# -- Anonymous Function --
-# ------------------------
-# [1] It Has No Name
-# [2] You Can Call It Inline Without Defining It
-# [3] You Can Use It In Return Data From Another Function
-# [4] Lambda Used For Simple Functions and Def Handle The Large Tasks
-# [5] Lambda is One Single Expression not Block Of Code
-# [6] Lambda Type is Function
-# -------------------------------------------------------------------
+# Assignment from 60 to 64
 
-def say_hello(name, age) : return f"Hello {name} your Age Is: {age}"
+# Assignment One
+def get_score(**techs):
+  for techKey, techVal in techs.items():
+    print(f'"{techKey} => {techVal}"')
 
-hello = lambda name, age : f"Hello {name} your Age Is: {age}"
-
-print(say_hello("Ahmed", 36))
-print(hello("Ahmed", 36))
-
-print(say_hello.__name__)
-print(hello.__name__)
-
-print(type(hello))
+get_score(Math=90, Science=80, Language=70)
+get_score(Logic=70, Problems=60)
