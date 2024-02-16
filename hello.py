1d21d77a4fee1e7aa7cc45d2e8703c133ed8cb54
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-16 19:33:23 +0200

    Built In Functions Lesson 6

--- hello.py
+++ hello.py
@@ -1,49 +1,26 @@
 # ----------------------------------
-# -- Built In Functions => Filter --
+# -- Built In Functions => Reduce --
 # ----------------------------------
-# [1] Filter Take A Function + Iterator
-# [2] Filter Run A Function On Every Element
-# [3] The Function Can Be Pre-Defined Function or Lambda Function
-# [4] Filter Out All Elements For Which The Function Return True
-# [5] The Function Need To Return Boolean Value
+# [1] Reduce Take A Function + Iterator
+# [2] Reduce Run A Function On FIrst and Second Element And Give Result
+# [3] Then Run Function On Result And Third Element
+# [4] Then Run Function On Rsult And Fourth Element And So On
+# [5] Till One ELement is Left And This is The Result of The Reduce
+# [6] The Function Can Be Pre-Defined Function or Lambda Function
 # ---------------------------------------------------------------
 
-# Example 1
+from functools import reduce
 
-def checkNumber(num):
+def sumAll(num1, num2):
 
-  return num > 10
+  return num1 + num2
 
-myNumbers = [0, 0, 1, 19, 10, 20, 100, 5, 0]
+numbers = [1, 8, 2, 9, 100]
 
-myResult = filter(checkNumber, myNumbers)
+result = reduce(sumAll, numbers)
 
-for number in myResult:
+result = reduce(lambda num1, num2: num1 + num2, numbers)
 
-  print(number)
+print(result)
 
-print("#" * 50)
-
-# Example 2
-
-def checkName(name):
-
-  return name.startswith("O")
-
-myTexts = ["Osama", "Omer", "Omar", "Ahmed", "Sayed", "Othman"]
-
-myReturnedData = filter(checkName, myTexts)
-
-for person in myReturnedData:
-
-  print(person)
-
-print("#" * 50)
-
-# Example 3
-
-myNames = ["Osama", "Omer", "Omar", "Ahmed", "Sayed", "Othman", "Ameer"]
-
-for p in filter(lambda name: name.startswith("A"), myNames):
-
-  print(p)
+# ((((1 + 8) + 2) + 9) + 100)
