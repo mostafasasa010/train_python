f221972d5f47cfcc7ec0d3128c526dc2adc944b7
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-16 19:28:02 +0200

    Built In Functions Lesson 5

--- hello.py
+++ hello.py
@@ -1,37 +1,49 @@
-# -------------------------------
-# -- Built In Functions => Map --
-# -------------------------------
-# [1] Map Take A Function + Iterator
-# [2] Map Called Map Because It Map The Function On Every Element
+# ----------------------------------
+# -- Built In Functions => Filter --
+# ----------------------------------
+# [1] Filter Take A Function + Iterator
+# [2] Filter Run A Function On Every Element
 # [3] The Function Can Be Pre-Defined Function or Lambda Function
+# [4] Filter Out All Elements For Which The Function Return True
+# [5] The Function Need To Return Boolean Value
 # ---------------------------------------------------------------
 
-# Use Map With Predefined Function
+# Example 1
 
-def formatText(text):
+def checkNumber(num):
 
-  return f"- {text.strip().capitalize()} -"
+  return num > 10
 
-myTexts = [" OSama ", "AHMED", "  sAYed  "]
+myNumbers = [0, 0, 1, 19, 10, 20, 100, 5, 0]
 
-myFormatedData = map(formatText, myTexts)
+myResult = filter(checkNumber, myNumbers)
 
-print(myFormatedData)
+for number in myResult:
 
-for name in list(map(formatText, myTexts)):
-
-  print(name)
+  print(number)
 
 print("#" * 50)
 
-# Use Map With Lambda Function
+# Example 2
 
-def formatText(text):
+def checkName(name):
 
-  return f"- {text.strip().capitalize()} -"
+  return name.startswith("O")
 
-myTexts = [" OSama ", "AHMED", "  sAYed  "]
+myTexts = ["Osama", "Omer", "Omar", "Ahmed", "Sayed", "Othman"]
 
-for name in list(map((lambda text: f"- {text.strip().capitalize()} -"), myTexts)):
+myReturnedData = filter(checkName, myTexts)
 
-  print(name)
+for person in myReturnedData:
+
+  print(person)
+
+print("#" * 50)
+
+# Example 3
+
+myNames = ["Osama", "Omer", "Omar", "Ahmed", "Sayed", "Othman", "Ameer"]
+
+for p in filter(lambda name: name.startswith("A"), myNames):
+
+  print(p)
