57a7cfe05fafa9f97c3f9fee39c433de39df581f
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-16 18:19:28 +0200

    Files Lesson 3

--- hello.py
+++ hello.py
@@ -1,33 +1,18 @@
-# --------------------------------
-# -- File Handling => Read File --
-# --------------------------------
+# -----------------------------------------------
+# -- File Handling => Write and Append In File --
+# -----------------------------------------------
 
-# myFile = open("D:\Python\Files\osama.txt", "r")
+# myFile = open("D:\Python\Files\osama.txt", "w")
+# myFile.write("Hello\n")
+# myFile.write("Third Line")
 
-# print(myFile)  # File Data Object
-# print(myFile.name)
-# print(myFile.mode)
-# print(myFile.encoding)
+# myFile = open(r"D:\Python\Files\fun.txt", "w")
+# myFile.write("Elzero Web School\n" * 1000)
 
-# print(myFile.read())
-# print(myFile.read(5))
+# myList = ["Oasma\n", "Ahmed\n", "Sayed\n"]
 
-# print(myFile.readline(5))
-# print(myFile.readline())
-# print(myFile.readline())
+# myFile = open("D:\Python\Files\osama.txt", "w")
+# myFile.writelines(myList)
 
-# print(myFile.readlines())
-# print(myFile.readlines(50))
-# print(type(myFile.readlines()))
-
-# for line in myFile:
-
-#   print(line)
-
-#   if line.startswith("07"):
-
-#     break
-
-# # Close The File
-
-# myFile.close()
+# myFile = open("D:\Python\Files\osama.txt", "a")
+# myFile.write("Elzero")
