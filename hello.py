fc368d2e007c898a76480ca95fc85729ea6e34bd
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-16 18:24:21 +0200

    Files Lesson 4

--- hello.py
+++ hello.py
@@ -1,18 +1,17 @@
-# -----------------------------------------------
-# -- File Handling => Write and Append In File --
-# -----------------------------------------------
+# -------------------------------------
+# -- File Handling => Important Info --
+# -------------------------------------
 
-# myFile = open("D:\Python\Files\osama.txt", "w")
-# myFile.write("Hello\n")
-# myFile.write("Third Line")
-
-# myFile = open(r"D:\Python\Files\fun.txt", "w")
-# myFile.write("Elzero Web School\n" * 1000)
-
-# myList = ["Oasma\n", "Ahmed\n", "Sayed\n"]
-
-# myFile = open("D:\Python\Files\osama.txt", "w")
-# myFile.writelines(myList)
+# import os
 
 # myFile = open("D:\Python\Files\osama.txt", "a")
-# myFile.write("Elzero")
+# myFile.truncate(5)
+
+# myFile = open("D:\Python\Files\osama.txt", "a")
+# print(myFile.tell())
+
+# myFile = open("D:\Python\Files\osama.txt", "r")
+# myFile.seek(11)
+# print(myFile.read())
+
+# os.remove("D:\Python\Files\osama.txt")
