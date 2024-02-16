df25fe4d1e8c3a495cf761609c6e068546f69148
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-16 19:52:57 +0200

    Assignment from 69 to 71 => 1

--- hello.py
+++ hello.py
@@ -1,41 +1,20 @@
-# ------------------------
-# -- Built In Functions --
-# ------------------------
-# enumerate()
-# help()
-# reversed()
-# ------------------------
+values = (0, 1, 2)
 
-# enumerate(iterable, start=0)
+if any(values):
+  # my_var become 0 because values included 1, 2 becouse those are true
+  my_var = 0
 
-mySkills = ["Html", "Css", "Js", "PHP"]
+my_list = [True, 1,  1, ["A", "B"], 10.5, my_var]
 
-mySkillsWithCounter = enumerate(mySkills, 20)
+# my_list[:4] = true
+# my_list[:6] = false
+# my_list[:] = false
+if all(my_list[:4]) or all(my_list[:6]) or all(my_list[:]):
+  # print Good because my_list[:4] = true
+  print("Good")
 
-print(type(mySkillsWithCounter))
+else:
 
-for counter, skill in mySkillsWithCounter:
+  print("Bad")
 
-  print(f"{counter} - {skill}")
-
-print("#" * 50)
-
-# help()
-
-# print(help(print))
-
-print("#" * 50)
-
-# reversed(iterable)
-
-myString = "Elzero"
-
-print(reversed(myString))
-
-for letter in reversed(myString):
-
-  print(letter)
-
-for s in reversed(mySkills):
-
-  print(s)
+# Output = Good
