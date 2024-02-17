7f07a810a488f50d59026f48803ad824d5a4bd61
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-17 17:10:19 +0200

    Assignment from 69 to 71 => 3

--- hello.py
+++ hello.py
@@ -1,5 +1,47 @@
-v = 40
+def my_all(list):
+  re = True
+  for item in list:
+    if not item:
+      re = False
+    else:
+      re = True
+  return re
 
-my_range = list(range(v))
+def my_any(list):
+  re = False
+  for item in list:
+    if item:
+      re = True
+  return re
 
-print(sum(my_range, v) + pow(v, v, v))  # 820
+def my_min(list):
+  if len(list) < 2:
+    return list[0]
+  if list[1] < list[0]:
+    return my_min(list[1:])
+  else:
+    return my_min(list[:1] + list[2:])
+
+def my_max(list):
+  if len(list) < 2:
+    return list[0]
+  if list[1] > list[0]:
+    return my_max(list[1:])
+  else:
+    return my_max(list[:1] + list[2:])
+
+# my_all
+print(my_all([1, 2, 3])) # True
+print(my_all([1, 2, 3, []])) # False
+
+# my_any
+print(my_any([0, 1, [], False])) # True
+print(my_any([(), 0, False])) # False
+
+# my_min
+print(my_min([10, 100, -20, -100, 50])) # -100
+print(my_min((10, 100, -20, -100, 50))) # -100
+
+# my_max
+print(my_max([10, 100, -20, -100, 50, 700])) # 700
+print(my_max((10, 100, -20, -100, 50, 700))) # 700
