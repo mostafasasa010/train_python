8d85d2d879e4abc2e390d1b646970727c71c9154
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-17 20:09:04 +0200

    Assignment from 51 to 55 => 3

--- hello.py
+++ hello.py
@@ -1,28 +1,30 @@
-i = 1
-while i <= 20:
-  if i == 6 or i == 8 or i == 12:
-    i += 1
-  print(f"0{i}" if i < 10 else f"{i}")
-  i += 1
+# Input
+my_ranks = {
+  'Math': 'A',
+  "Science": 'B',
+  'Drawing': 'A',
+  'Sports': 'C'
+}
+result = 0
+
+for rankKey, rankValue in my_ranks.items():
+  val = 0
+  if rankValue == "A":
+    result += 100
+    val = 100
+  elif rankValue == "B":
+    result += 80
+    val = 80
+  elif rankValue == "C":
+    result += 40
+    val = 40
+  print(f"My Rank in {rankKey} Is {rankValue} And This Equal {val} Points")
 else:
-  print("All Numbers Printed")
+  print(f"Total Points Is {result}")
 
 # Needed Output
-# "01"
-# "02"
-# "03"
-# "04"
-# "05"
-# "07"
-# "09"
-# "10"
-# "11"
-# "13"
-# "14"
-# "15"
-# "16"
-# "17"
-# "18"
-# "19"
-# "20"
-# "All Numbers Printed"
+# "My Rank in Math Is A And This Equal 100 Points"
+# "My Rank in Science Is B And This Equal 80 Points"
+# "My Rank in Drawing Is A And This Equal 100 Points"
+# "My Rank in Sports Is C And This Equal 40 Points"
+# "Total Points Is 320"
