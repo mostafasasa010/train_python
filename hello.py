6c0b65b71b598bf1c34aa2604d9b278f27f152b0
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-18 17:46:15 +0200

    Date And Time Lesson 1

--- hello.py
+++ hello.py
@@ -1,20 +1,69 @@
-# ------------------------------------------
-# -- Modules => Install External Packages --
-# ------------------------------------------
-# [1] Module vs Package
-# [2] External Packages Downloaded From The Internet
-# [3] You Can Install Packages With Python Package Manager PIP
-# [4] PIP Install the Package and Its Dependencies
-# [5] Modules List "https://docs.python.org/3/py-modindex.html"
-# [6] Packages and Modules Directory "https://pypi.org/"
-# [7] PIP Manual "https://pip.pypa.io/en/stable/reference/pip_install/"
-# ---------------------------------------------------------------------
+# -----------------------------------
+# -- Date and Time => Introduction --
+# -----------------------------------
 
-# import termcolor
-# import pyfiglet
+import datetime
 
-# print(dir(pyfiglet))
-# print(pyfiglet.figlet_format("Elzero"))
-# print(termcolor.colored("Elzero", color="yellow"))
+# print(dir(datetime))
+# print(dir(datetime.datetime))
 
-# print(termcolor.colored(pyfiglet.figlet_format("Elzero"), color="yellow"))
+# Print The Current Date and Time
+print(datetime.datetime.now())
+
+print("#" * 40)
+
+# Print The Current Year
+print(datetime.datetime.now().year)
+
+# Print The Current Month
+print(datetime.datetime.now().month)
+
+# Print The Current Day
+print(datetime.datetime.now().day)
+
+print("#" * 40)
+
+# Print Start and End Of Date
+print(datetime.datetime.min)
+print(datetime.datetime.max)
+
+print("#" * 40)
+
+# print(dir(datetime.datetime.now()))
+
+# Print The Current Time
+print(datetime.datetime.now().time())
+
+print("#" * 40)
+
+# Print The Current Time Hour
+print(datetime.datetime.now().time().hour)
+
+# Print The Current Time Minute
+print(datetime.datetime.now().time().minute)
+
+# Print The Current Time Second
+print(datetime.datetime.now().time().second)
+
+print("#" * 40)
+
+# Print Start and End Of Time
+print(datetime.time.min)
+print(datetime.time.max)
+
+print("#" * 40)
+
+# Print Specific Date
+print(datetime.datetime(1982, 10, 25))
+print(datetime.datetime(1982, 10, 25, 10, 45, 55, 150364))
+
+print("#" * 40)
+
+myBirthDay = datetime.datetime(2004, 12, 27)
+dateNow = datetime.datetime.now()
+
+print(f"My Birthday is {myBirthDay} And ", end="")
+print(f"Date Now Is {dateNow}")
+
+print(f" I Lived For {dateNow - myBirthDay}")
+print(f" I Lived For {(dateNow - myBirthDay).days} Days.")
