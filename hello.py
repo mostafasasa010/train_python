16356b37599c19d8a5529e95625940c3bdab62bf
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-18 17:21:58 +0200

    Modules Lesson 1

--- hello.py
+++ hello.py
@@ -1,76 +1,22 @@
-# Input
-students = {
-  "Ahmed": {
-    "Math": "A",
-    "Science": "D",
-    "Draw": "B",
-    "Sports": "C",
-    "Thinking": "A"
-  },
-  "Sayed": {
-    "Math": "B",
-    "Science": "B",
-    "Draw": "B",
-    "Sports": "D",
-    "Thinking": "A"
-  },
-  "Mahmoud": {
-    "Math": "D",
-    "Science": "A",
-    "Draw": "A",
-    "Sports": "B",
-    "Thinking": "B"
-  }
-}
+# ---------------------------------
+# -- Modules => Built In Modules --
+# ---------------------------------
+# [1] Module is A File Contain A Set Of Functions
+# [2] You Can Import Module in Your App To Help You
+# [3] You Can Import Multiple Modules
+# [4] You Can Create Your Own Modules
+# [5] Modules Saves Your Time
+# --------------------------------------------------
 
-for studentName, studentVals in students.items():
-  re = 0
-  print("------------------------------")
-  print(f"-- Student name => {studentName}")
-  print("------------------------------")
-  for key, vla in studentVals.items():
-    r = 0
-    if vla == "A":
-      r = 100
-      re += 100
-    elif vla == "B":
-      r = 80
-      re += 80
-    elif vla == "C":
-      r = 40
-      re += 40
-    elif vla == "D":
-      r = 20
-      re += 20
-    print(f"- {key} => {r} Points")
-  else:
-    print(f"Total Points For {studentName} Is {re}")
+# Import Main Module
+import random
+print(random)
+print(f"Print Random Float Number {random.random()}")
 
-# Needed Output
-# "------------------------------"
-# "-- Student Name => Ahmed"
-# "------------------------------"
-# "- Math => 100 Points"
-# "- Science => 20 Points"
-# "- Draw => 80 Points"
-# "- Sports => 40 Points"
-# "- Thinking => 100 Points"
-# "Total Points For Ahmed Is 340"
-# "------------------------------"
-# "-- Student Name => Sayed"
-# "------------------------------"
-# "- Math => 80 Points"
-# "- Science => 80 Points"
-# "- Draw => 80 Points"
-# "- Sports => 20 Points"
-# "- Thinking => 100 Points"
-# "Total Points For Sayed Is 360"
-# "------------------------------"
-# "-- Student Name => Mahmoud"
-# "------------------------------"
-# "- Math => 20 Points"
-# "- Science => 100 Points"
-# "- Draw => 100 Points"
-# "- Sports => 80 Points"
-# "- Thinking => 80 Points"
-# "Total Points For Mahmoud Is 380"
+# Show All Functions Inside Module
+print(dir(random))
+
+# Import One Or Two Functions From Module
+from random import randint, random
+print(f"Print Random Float {random()}")
+print(f"Print Random Integer {randint(100, 900)}")
