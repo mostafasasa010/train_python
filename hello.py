a1a20a99b15539da198dc2a55b40c9eb477cbc1d
mostafasasa010 <mostafa.sasa20004@gmail.com>
2024-02-19 12:34:36 +0200

    Decorators Lesson Three

--- hello.py
+++ hello.py
@@ -1,34 +1,51 @@
-# --------------------------------------------
-# -- Decorators => Function With Parameters --
-# --------------------------------------------
+# ----------------------------------------
+# -- Decorators => Practical Speed Test --
+# ----------------------------------------
+
+from time import time
 
 def myDecorator(func):  # Decorator
 
-  def nestedFunc(num1, num2):  # Any Name Its Just For Decoration
+  def nestedFunc(*numbers):  # Any Name Its Just For Decoration
 
-    if num1 < 0 or num2 < 0:
+    for number in numbers:
 
-      print("Beware One Of The Numbers Is Less Than Zero")
+      if number < 0:
 
-    func(num1, num2)  # Execute Function
+        print("Beware One Of The Numbers Is Less Than Zero")
 
-  return nestedFunc  # Return All Data
-
-def myDecoratorTwo(func):  # Decorator
-
-  def nestedFunc(num1, num2):  # Any Name Its Just For Decoration
-
-    print("Coming From Decorator Two")
-
-    func(num1, num2)  # Execute Function
+    func(*numbers)  # Execute Function
 
   return nestedFunc  # Return All Data
 
 @myDecorator
-@myDecoratorTwo
 
-def calculate(n1, n2):
+def calculate(n1, n2, n3, n4):
 
-  print(n1 + n2)
+  print(n1 + n2 + n3 + n4)
 
-calculate(-5, 90)
+calculate(-5, 90, 50, 150)
+
+def speedTest(func):
+
+  def wrapper():
+
+    start = time()
+
+    func()
+
+    end = time()
+
+    print(f"Function Running Time Is: {end - start}")
+
+  return wrapper
+
+@speedTest
+
+def bigLoop():
+
+  for number in range(1, 20000):
+
+    print(number)
+
+bigLoop()
